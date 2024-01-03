1890094589f25939d9a13a0e5d45bd460a862f27
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-03 16:28:39 +0800

    feat: add navbar, logo

---
+++ app/components/Logo.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <main className="flex gap-3 items-center">
+      <span className="flex h-10 w-10 cursor-default items-center justify-center rounded-full bg-white align-middle text-gray-950 hover:rotate-180 transition-all duration-500">
+        A
+      </span>
+      <section className="item-center flex cursor-pointer text-xl font-black tracking-wide">
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          I
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          z
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          z
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          a
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          h
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          n
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          i
+        </span>
+        <span className="transition-all duration-500 hover:-mt-2 hover:text-sky-500 hover:duration-100">
+          n
+        </span>
+      </section>
+    </main>
+  );
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import Logo from "./Logo";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <Logo />
+      <ul className="flex gap-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
