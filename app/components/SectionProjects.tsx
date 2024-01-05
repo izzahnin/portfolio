1019b2c5ef14dd791dcc3eb12ef1690dc163ccbf
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-05 22:59:10 +0800

    feat: fetch data from local

--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import Image from 'next/image'
-import CardProject from './CardProject'
+import React from "react";
+import Image from "next/image";
+import CardProject from "./CardProject";
+import { getAllData } from "../data";
 
 export default function SectionProjects() {
   return (
-    <main className=''>
-      <section>
-        <h1>Here are a few of my favorite projects.</h1>
-        <span></span>
+    <main className="flex flex-col gap-5">
+      <section className="flex flex-col-reverse items-center gap-4 md:flex-row">
+        <h1 className="min-w-max overflow-hidden  text-center text-3xl font-bold md:text-left">
+          Here are a few of my favorite projects.
+        </h1>
+        <span className="h-1 w-1/4 flex-grow bg-blue-primary  md:w-full"></span>
       </section>
-      <section className='grid grid-cols-3 gap-4'>
-        <CardProject />
-        <CardProject />
-        <CardProject />
+      <section className="grid gap-4 lg:grid-cols-3">
+        {getAllData().slice(0,3).map((project) => (
+          <CardProject
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            linkGithub={project.linkGithub}
+            linkDemo={project.linkDemo}
+            thumbnail={project.thumbnail}
+            tools={project.tools}
+          />
+        ))}
       </section>
+      
     </main>
-  )
+  );
 }
