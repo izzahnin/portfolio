6ee5ccf0c4fcef5cb1e5a5a0b7922616a802a417
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-05 22:55:59 +0800

    feat: move to folder page

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
+import "../globals.css";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-auto">
       <body className="${inter.className} flex-1 bg-blue-dark">
-        <main className="max-w-6xl m-auto p-5 text-white">
+        <main className="m-auto max-w-6xl p-3 sm:p-5 text-white">
           <Navbar />
           {children}
           <Footer />
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import SectionProjects from "../components/SectionProjects";
+import SectionTools from "../components/SectionTools";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <SectionProjects />
+      <SectionTools />
+    </main>
+  );
+}
