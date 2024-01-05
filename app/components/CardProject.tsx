d10a17f72764bb17272001e9a34adb6cddb8f904
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-05 22:59:22 +0800

    feat: add list tools

--- app/components/CardProject.tsx
+++ app/components/CardProject.tsx
@@ -2,29 +2,57 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CardProject() {
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+
+interface CardProjectProps {
+  title: string;
+  description: string;
+  linkGithub: string;
+  linkDemo: string;
+  thumbnail: string;
+  tools: string[];
+}
+
+export default function CardProject(props: CardProjectProps) {
+  const { title, description, linkGithub, linkDemo, thumbnail, tools } = props;
   return (
-    <main className="mx-auto flex max-w-sm flex-col md:justify-center gap-5">
-      <Link href="https://github.com/izzahnin">
-        <section className="rounded-xl border border-gray-300 p-2 transition-all delay-75 duration-200 hover:border-blue-primary ">
+    <main className="mx-auto flex max-w-sm max-h-min flex-col gap-5 md:justify-center">
+      <Link href={linkDemo} target="_blank">
+        <section className=" relative flex max-h-min max-w-sm flex-col justify-center rounded-xl border border-gray-300 p-2 transition-all delay-75 duration-200 hover:-translate-y-2 hover:border-blue-primary hover:opacity-75">
           <Image
-            src="/image/bg-1.svg"
+            src={thumbnail}
             alt="Project 1"
-            width={300}
+            width={100}
             height={100}
-            className="h-60 w-full rounded-md"
+            className="h-full  w-full rounded-md object-cover"
           />
         </section>
       </Link>
       <section>
-        <h3 className="font-bold text-lg">Lorem, ipsum dolor.</h3>
-        <p className="text-slate-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis commodi
-          quos architecto earum sint.
-        </p>
-        <div>
-          
+        <div className="flex">
+          <div>
+            <h3 className="text-lg font-bold">{title}</h3>
+            <p className="text-sm text-slate-500">{description}</p>
+          </div>
+          <div className="flex gap-2 text-blue-primary">
+            <Link href={linkDemo}>
+              <FiExternalLink />
+            </Link>
+            <Link href={linkGithub}>
+              <FiGithub />
+            </Link>
+          </div>
         </div>
+        <ul className="my-2 flex gap-2 text-sm">
+          {tools.map((tool) => (
+            <li
+              className="cursor-default rounded-lg bg-blue-950 px-2 py-1 hover:opacity-70"
+              key={tool}
+            >
+              {tool}
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
