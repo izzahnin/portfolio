d3bdba1208f505bc377cf088a35926eb55c8bddd
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-09 23:04:05 +0800

    feat: update list icon

--- app/utils/listIcon.ts
+++ app/utils/listIcon.ts
@@ -1,47 +1,56 @@
 export const listIcon = [
   {
     id: 1,
-    name: "HTML5",
+    name: "HTML",
     svg: "/icons/html5.svg",
+    color: "#E34F26",
   },
   {
     id: 2,
     name: "CSS",
     svg: "/icons/css.svg",
+    color: "#1572B6",
   },
   {
     id: 3,
     name: "JavaScript",
     svg: "/icons/javascript.svg",
+    color: "#F7DF1E",
   },
   {
     id: 4,
     name: "React",
     svg: "/icons/react.svg",
+    color: "#61DAFB",
   },
   {
     id: 5,
     name: "NodeJS",
     svg: "/icons/nodejs.svg",
+    color: "#339933",
   },
   {
     id: 6,
     name: "TypeScript",
     svg: "/icons/typescript.svg",
+    color: "#007ACC",
   },
   {
     id: 7,
     name: "TailwindCSS",
     svg: "/icons/tailwindcss.svg",
+    color: "#38B2AC",
   },
   {
     id: 8,
     name: "NextJS",
     svg: "/icons/nextjs.svg",
+    color: "#ffffff",
   },
   {
     id: 9,
     name: "Git",
     svg: "/icons/git.svg",
+    color: "#F05032",
   }
 ];
