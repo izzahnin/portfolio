01db96cdbe99ba1269ebcdab187d71cd6a08ab2b
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-09 23:05:41 +0800

    feat: add list project, and title of section

--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -1,31 +1,32 @@
 import React from "react";
-import Image from "next/image";
 import CardProject from "./CardProject";
 import { getAllData } from "../data";
 
 export default function SectionProjects() {
   return (
     <main className="flex flex-col gap-5">
-      <section className="flex flex-col-reverse items-center gap-4 md:flex-row">
-        <h1 className="min-w-max overflow-hidden  text-center text-3xl font-bold md:text-left">
-          Here are a few of my favorite projects.
-        </h1>
-        <span className="h-1 w-1/4 flex-grow bg-blue-primary  md:w-full"></span>
+      <section className="flex flex-col items-center gap-4 text-5xl md:flex-row">
+        <div className="flex items-center gap-2">
+          <h1 className="font-semibold">Projects</h1>
+          <span className="h-3 w-3 rounded-full bg-blue-primary"></span>
+        </div>
+        <p></p>
       </section>
       <section className="grid gap-4 lg:grid-cols-3">
-        {getAllData().slice(0,3).map((project) => (
-          <CardProject
-            key={project.id}
-            title={project.title}
-            description={project.description}
-            linkGithub={project.linkGithub}
-            linkDemo={project.linkDemo}
-            thumbnail={project.thumbnail}
-            tools={project.tools}
-          />
-        ))}
+        {getAllData()
+          .slice(0, 7)
+          .map((project) => (
+            <CardProject
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              linkGithub={project.linkGithub}
+              linkDemo={project.linkDemo}
+              thumbnail={project.thumbnail}
+              tools={project.tools}
+            />
+          ))}
       </section>
-      
     </main>
   );
 }
--- app/components/SectionTools.tsx
+++ app/components/SectionTools.tsx
@@ -5,19 +5,30 @@ import { listIcon } from "../utils/listIcon";
 
 export default function SectionTools() {
   return (
-    <main className="grid w-full grid-cols-3 gap-4">
-      {listIcon.map((icon) => (
-        <div key={icon.id} className="flex flex-col items-center gap-1">
-          <Image
-            src={icon.svg}
-            alt={icon.name}
-            width={40}
-            height={40}
-            className="h-10 w-10"
-          />
-          <span className="text-center text-xs font-semibold text-gray-300">{icon.name}</span>
+    <main className="flex flex-col gap-5">
+      <section className="flex flex-col items-center gap-4 text-5xl md:flex-row">
+        <div className="flex items-center gap-2">
+          <h1 className="font-semibold">Tools</h1>
+          <span className="h-3 w-3 rounded-full bg-blue-primary"></span>
         </div>
-      ))}
+        <p></p>
+      </section>
+      <section className="grid w-full grid-cols-3 gap-4">
+        {listIcon.map((icon) => (
+          <div key={icon.id} className="flex flex-col items-center gap-1">
+            <Image
+              src={icon.svg}
+              alt={icon.name}
+              width={40}
+              height={40}
+              className="h-10 w-10"
+            />
+            <span className="text-center text-xs font-semibold text-gray-300">
+              {icon.name}
+            </span>
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
