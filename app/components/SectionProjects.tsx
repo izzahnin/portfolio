0b89032e5791cb24da45471a3a71f011593099b5
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-14 23:48:30 +0800

    feat: add responsive design

--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -4,29 +4,27 @@ import { getAllData } from "../data";
 
 export const SectionProjects = () => {
   return (
-    <main  id="projects" className="flex flex-col gap-5">
-      <section className="flex flex-col items-center gap-4 text-5xl md:flex-row">
+    <main id="projects" className="m-auto flex max-w-6xl flex-col gap-5 px-4">
+      <section className="flex flex-col items-center gap-4 text-4xl md:flex-row">
         <div className="flex items-center gap-2">
           <h1 className="font-semibold">Projects</h1>
           <span className="h-3 w-3 rounded-full bg-blue-primary"></span>
         </div>
         <p></p>
       </section>
-      <section className="grid gap-4 lg:grid-cols-3 items-start ">
-        {getAllData()
-          .slice(0, 7)
-          .map((project) => (
-            <CardProject
-              key={project.id}
-              title={project.title}
-              description={project.description}
-              linkGithub={project.linkGithub}
-              linkDemo={project.linkDemo}
-              thumbnail={project.thumbnail}
-              tools={project.tools}
-            />
-          ))}
+      <section className="grid items-start gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+        {getAllData().map((project) => (
+          <CardProject
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            linkGithub={project.linkGithub}
+            linkDemo={project.linkDemo}
+            thumbnail={project.thumbnail}
+            tools={project.tools}
+          />
+        ))}
       </section>
     </main>
   );
-}
+};
--- app/components/SectionTools.tsx
+++ app/components/SectionTools.tsx
@@ -5,30 +5,35 @@ import { listIcon } from "../utils/listIcon";
 
 export const SectionTools = () => {
   return (
-    <main className="flex flex-col gap-5 max-w-3xl">
-      <section className="flex flex-col items-center gap-4 text-5xl md:flex-row">
-        <div className="flex items-center gap-2">
+    <main
+      id="tools"
+      className="m-auto flex w-full max-w-sm flex-col gap-8 px-4 sm:px-0 md:max-w-3xl lg:max-w-6xl lg:px-4  "
+    >
+      <section className="flex flex-col items-center gap-4 md:items-start ">
+        <div className="flex items-center gap-2 text-4xl">
           <h1 className="font-semibold">Tools</h1>
           <span className="h-3 w-3 rounded-full bg-blue-primary"></span>
         </div>
-        <p></p>
+        <p>Tech stack and tools that i use</p>
       </section>
-      <section className="grid w-full grid-cols-3 gap-4">
+      <section className="grid h-full grid-cols-3 gap-10  sm:grid-cols-4 md:gap-14">
         {listIcon.map((icon) => (
-          <div key={icon.id} className="flex flex-col items-center gap-1">
-            <Image
-              src={icon.svg}
-              alt={icon.name}
-              width={40}
-              height={40}
-              className="h-10 w-10 hover:scale-110 transition duration-300 ease-in-out"
-            />
-            <span className="text-center text-xs font-semibold text-gray-300 cursor-default">
+          <section key={icon.id} className="flex flex-col items-center gap-5">
+            <div className="flex h-20 w-20 flex-col items-center  justify-center gap-1 rounded-full border border-transparent bg-[#092039] md:h-24 md:w-24 ">
+              <Image
+                src={icon.svg}
+                alt={icon.name}
+                width={40}
+                height={40}
+                className="h-10 w-10 transition duration-300 ease-in-out hover:scale-110 md:h-14 md:w-14"
+              />
+            </div>
+            <span className="cursor-default text-center text-xs font-semibold text-gray-300 md:text-sm">
               {icon.name}
             </span>
-          </div>
+          </section>
         ))}
       </section>
     </main>
   );
-}
+};
