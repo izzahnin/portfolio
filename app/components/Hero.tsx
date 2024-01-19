b0d722a4c0641276a58c95a8540a59fad4d7a09d
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2024-01-19 22:42:40 +0800

    feat: style hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+
+import "../globals.css";
+import { IoIosArrowDown } from "react-icons/io";
 
 export const Hero = () => {
   return (
-    <main className='text-center flex flex-col items-center justify-center h-96 my-auto'>
-      {/* <section> */}
-        <span>{`Hey, I'm Nurul Izzah`}</span>
-        <p className='text-3xl font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      {/* </section> */}
+    <main className="custom-height m-auto my-auto flex w-full max-w-6xl flex-col items-center justify-center text-center px-4">
+      <section className="flex h-3/4 items-center justify-center">
+        <div>
+          <h1 className="text-3xl font-black">
+            Hi, I&apos;m <span className="text-blue-primary">Nurul Izzah</span>
+          </h1>
+          <p className="text-3xl font-bold">I&apos;m a Frontend Developer</p>
+        </div>
+      </section>
+      <section className="">
+        <Link href="#about">
+          <IoIosArrowDown className="animate-bounce text-5xl" />
+        </Link>
+      </section>
     </main>
-  )
-}
+  );
+};
